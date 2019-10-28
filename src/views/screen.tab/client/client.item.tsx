e97f76a32839c184f3f6dc4cf2cb279e00421dcc
leyue <317513461@qq.com>
2019-10-28 16:54:05 +0800

    设置压测时间

--- src/views/screen.tab/client/client.item.tsx
+++ src/views/screen.tab/client/client.item.tsx
@@ -4,7 +4,8 @@ import {NavigationScreenProp} from 'react-navigation';
 import {IAccount, ITask} from '../../../stores';
 import moment from 'moment';
 import {View, StyleSheet, Text, ProgressBarAndroid} from 'react-native';
-import {Card} from 'native-base';
+import {Card, Button} from 'native-base';
+import DatePicker from 'react-native-datepicker';
 import {OlView, CollapseVItem, DlgConfirm} from '../../../components';
 
 type IProps = {
@@ -13,13 +14,26 @@ type IProps = {
 
 @observer
 class ClientItem extends Component<IProps> {
-  public state: any = {};
+  public state: any = {
+    sTime: '',
+    eTime: '',
+  };
 
   constructor(props: IProps) {
     super(props);
   }
 
   async componentDidMount() {}
+
+  async updateSchedule() {
+    let doc = this.props.data;
+    await $ax.post('/cliSchedule', {
+      action: 'update',
+      mac: doc.index.mac,
+      sTime: this.state.sTime,
+      eTime: this.state.eTime,
+    });
+  }
 
   render() {
     let doc = this.props.data;
@@ -28,6 +42,7 @@ class ClientItem extends Component<IProps> {
         {this.renderBasic()}
         {this.renderStatus()}
         {this.renderAbility()}
+        {this.renderSchedule()}
       </Card>
     );
   }
@@ -86,9 +101,13 @@ class ClientItem extends Component<IProps> {
     let progress = 0.0;
     if (status === 'busy') {
       color = $color.orange;
-      progress =
-        +doc.task._case.progress.slice(0, doc.task._case.progress.length - 1) /
-        100;
+      if (doc.task._case.progress) {
+        progress =
+          +doc.task._case.progress.slice(
+            0,
+            doc.task._case.progress.length - 1,
+          ) / 100;
+      }
     }
     return (
       <View style={{marginTop: 10}}>
@@ -139,6 +158,78 @@ class ClientItem extends Component<IProps> {
           <Text style={{width: '20%'}}>主机</Text>
           <Text style={{width: '80%'}}>{doc.index.mac}</Text>
           <View style={{flex: 2}} />
+        </View>
+      </View>
+    );
+  }
+
+  renderSchedule() {
+    return (
+      <View>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={{width: '20%'}}>压测时间</Text>
+          <DatePicker
+            style={{width: 90}}
+            date={this.state.sTime}
+            mode="time"
+            placeholder="输入开始时间"
+            format="HH:mm"
+            minDate="2016-05-01"
+            maxDate="2016-06-01"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            showIcon={false}
+            customStyles={{
+              dateIcon: {},
+              dateText: {},
+              dateInput: {
+                height: 30,
+                color: $color.wet_asphalt,
+                borderWidth: 0,
+              },
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={date => {
+              this.setState({sTime: date});
+            }}
+          />
+          <DatePicker
+            style={{width: 90}}
+            date={this.state.eTime}
+            mode="time"
+            placeholder="输入结束时间"
+            format="HH:mm"
+            minDate="2016-05-01"
+            maxDate="2016-06-01"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            showIcon={false}
+            customStyles={{
+              dateIcon: {},
+              dateText: {},
+              dateInput: {
+                height: 30,
+                color: $color.wet_asphalt,
+                borderWidth: 0,
+              },
+              // ... You can check the source to find the other keys.
+            }}
+            onDateChange={date => {
+              this.setState({eTime: date});
+            }}
+          />
+          <View style={{flex: 2}} />
+          <Button
+            style={{
+              backgroundColor: $color.wet_asphalt,
+              justifyContent: 'center',
+              height: 25,
+              paddingHorizontal: 5,
+            }}
+            disabled={this.state.uploading}
+            onPress={() => {}}>
+            <Text style={{color: $color.white, fontSize: 20}}>确认</Text>
+          </Button>
         </View>
       </View>
     );
